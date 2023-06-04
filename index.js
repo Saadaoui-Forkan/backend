9e9f2cd242ba38c18a816eb9d6526805fa058811
unknown <forkan.saadaoui@gmail.com>
2023-06-04 13:38:31 +0100

    upload images

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ app.use(express.json())
 app.use('/product', productRoute)
 app.use('/user', userRoute)
 
+app.use('/get_image', express.static('./uploads'))
+
 const PORT = 5000;
 app.listen(PORT, ()=> console.log(`Server Is Running On Port ${PORT}`))
--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,30 @@ const express = require('express')
 const router = express.Router()
 const Product = require("../models/product")
 
+const multer = require('multer')
+
+filename = '';
+
+const mystorage = multer.diskStorage({
+    destination: './uploads',
+    filename: (req, file, redirect) => {
+        let date = Date.now();
+        let fl = date + '.' +file.mimetype.split('/')[1];
+        redirect(null, fl);
+        filename= fl;
+    }
+})
+
+const upload = multer({storage: mystorage})
+
 // Post Method
-router.post("/create_product", async(req,res) => {
+router.post("/create_product", upload.any('image'), async(req,res) => {
     try {
         data = req.body
         const newProduct = new Product(data)
+        newProduct.image = filename
         savedProduct = await newProduct.save()
+        filename = ''
         res.status(200).send(savedProduct)
     } catch (error) {
         res.status(400).send(error)
